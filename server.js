e2b5537a87bf3198202b84ff7469847e2e14b7aa
mohamed ali <mohamedalihosni25@gmail.com>
2025-11-05 00:50:58 +0100

    feat: Add task management module with CRUD operations and task history tracking

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ import errorMiddleware from './src/shared/middlewares/error.middleware.js';
 import { PORT } from './src/shared/config/index.js';
 import { connectDB } from './src/shared/db/db_config.js';
 import authRouter from './src/modules/Authentication/index.js';
-// import teamARouter from './src/modules/Team_A/index.js';
+import teamARouter from './src/modules/Team_A/index.js';
+import teamCRouter from './src/modules/Team_C/index.js';
 
 const app = express();
 
@@ -18,7 +19,8 @@ app.use(errorMiddleware);
 await connectDB();
 
 app.use('/api', authRouter);
-// app.use('/api', teamARouter);
+app.use('/api', teamARouter);
+app.use('/api', teamCRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ src/modules/Team_C/models/task.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose"
+
+const { Schema, model } = mongoose
+
+const taskSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+  status: {
+    type: String,
+    enum: ["ToDo", "InProgress", "Standby", "Done"],
+    required: true
+  },
+  priority: {
+    type: String,
+    enum: ["Low", "Medium", "High"],
+    default: "Medium"
+  },
+  userStoryId: { type: Schema.Types.ObjectId, ref: "UserStory", required: true },
+  assignedTo: { type: Schema.Types.ObjectId, ref: "User" },
+}, { timestamps: true })
+
+taskSchema.index({ title: 1, userStoryId: 1 }, { unique: true })
+
+export default model("Task", taskSchema)
