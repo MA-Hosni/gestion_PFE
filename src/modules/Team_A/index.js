ab512544eec798719d366dbb0363fc4a5e694868
Mohamed Ali Hosni <124616606+MA-Hosni@users.noreply.github.com>
2025-11-25 08:16:09 +0100

    fix: Uncomment sprint routes in Team_A module

--- src/modules/Team_A/index.js
+++ src/modules/Team_A/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import projectRoutes from "./routes/project.routes.js";
-// import sprintRoutes from "./routes/sprint.routes.js";
+import sprintRoutes from "./routes/sprint.routes.js";
 
 const router = express.Router();
 
